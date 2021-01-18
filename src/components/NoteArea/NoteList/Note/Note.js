082485c3f5cd9c7658c41cffc645ab27f17970cb
raaynaldo <raaynaldo@hotmail.com>
2021-01-18 18:34:10 -0500

    body preview

--- src/components/NoteArea/NoteList/Note/Note.js
+++ src/components/NoteArea/NoteList/Note/Note.js
@@ -1,28 +1,26 @@
 import removeMd from "remove-markdown";
 
 const Note = ({ title, body, onClick }) => {
-  const titleSlice = removeMd(title).slice(0, 21);
+  const maxSizeTitle = 21;
+  const titleSlice = removeMd(title).slice(0, maxSizeTitle);
 
+  const maxSizeBody = 70;
   const findSpace = body.indexOf("\n");
   const newBody = findSpace != -1 ? body.slice(findSpace) : "";
-  const bodySlice = removeMd(newBody).slice(0, 65);
+  const bodySlice = removeMd(newBody).slice(0, maxSizeBody);
 
   return (
     <div
       className="flex flex-col h-20 px-5 py-2 space-y-1 text-red-500 bg-gray-100"
       onClick={onClick}
     >
-      {/* <b>React Router DOM</b>
-      <p className="text-xs">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit...
-      </p> */}
       <p className="font-bold">
         {titleSlice.length === 0
           ? "No Title"
-          : titleSlice + (titleSlice.length > 20 ? "..." : "")}
+          : titleSlice + (titleSlice.length >= maxSizeTitle ? "..." : "")}
       </p>
-      <p className="text-xs">
-        {bodySlice + (bodySlice.length > 64 ? "..." : "")}
+      <p className="text-xs break-all">
+        {bodySlice + (bodySlice.length >= maxSizeBody ? "..." : "")}
       </p>
     </div>
   );
