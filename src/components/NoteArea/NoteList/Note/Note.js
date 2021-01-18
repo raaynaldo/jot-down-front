db80c35db1f4881895a7ac188f2ef7d2fa295a7e
raaynaldo <raaynaldo@hotmail.com>
2021-01-18 17:49:49 -0500

    modify body preview

--- src/components/NoteArea/NoteList/Note/Note.js
+++ src/components/NoteArea/NoteList/Note/Note.js
@@ -2,18 +2,22 @@ import removeMd from "remove-markdown";
 
 const Note = ({ title, body, onClick }) => {
   const titleSlice = removeMd(title).slice(0, 21);
-  const bodySlice = removeMd(body).slice(0, 65);
+  const bodySlice = removeMd(body.slice(body.indexOf("\n"))).slice(0, 65);
 
   return (
     <div
-      className="flex flex-col h-20 px-5 py-3 space-y-1 text-red-500 bg-gray-100"
+      className="flex flex-col h-20 px-5 py-2 space-y-1 text-red-500 bg-gray-100"
       onClick={onClick}
     >
       {/* <b>React Router DOM</b>
       <p className="text-xs">
         Lorem ipsum dolor sit amet consectetur adipisicing elit...
       </p> */}
-      <b>{titleSlice.length === 0 ? "No Title" : titleSlice + (titleSlice.length > 20 ? "..." : "")}</b>
+      <p className="font-bold">
+        {titleSlice.length === 0
+          ? "No Title"
+          : titleSlice + (titleSlice.length > 20 ? "..." : "")}
+      </p>
       <p className="text-xs">
         {bodySlice + (bodySlice.length > 64 ? "..." : "")}
       </p>
