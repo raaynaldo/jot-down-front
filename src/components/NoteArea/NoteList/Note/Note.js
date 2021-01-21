0c57b08418b1b0e3d1a82188ccbb4c0edefd40ce
raaynaldo <raaynaldo@hotmail.com>
2021-01-20 23:53:11 -0500

    searching

--- src/components/NoteArea/NoteList/Note/Note.js
+++ src/components/NoteArea/NoteList/Note/Note.js
@@ -1,17 +1,38 @@
-import {useContext} from "react";
+import { useContext } from "react";
 import removeMd from "remove-markdown";
 import MainContext from "../../../../context/main/mainContext";
+import Highlighter from "react-highlight-words";
 
 const Note = ({ id, title, body, onClick, onContextMenu }) => {
+  const { activeNote, searchKeyNote } = useContext(MainContext);
   const maxSizeTitle = 21;
-  const titleSlice = removeMd(title).slice(0, maxSizeTitle);
+  const titleRemoveMd = removeMd(title);
+
+  const titleSlice = titleRemoveMd.slice(0, maxSizeTitle);
+  const titleElipsis =
+    titleSlice.length === 0
+      ? "No Title"
+      : titleSlice + (titleSlice.length === maxSizeTitle ? "..." : "");
 
   const maxSizeBody = 70;
   const findSpace = body.indexOf("\n");
   const newBody = findSpace !== -1 ? body.slice(findSpace) : "";
-  const bodySlice = removeMd(newBody).slice(0, maxSizeBody);
+  const bodyRemoveMd = removeMd(newBody);
+  let startBodySlice = 0;
+  if (searchKeyNote != "") {
+    startBodySlice = bodyRemoveMd.toLocaleLowerCase().indexOf(searchKeyNote);
+    startBodySlice =
+      startBodySlice < 5 && startBodySlice >= 0 ? 0 : startBodySlice - 5;
+  }
+  const bodySlice = bodyRemoveMd.slice(
+    startBodySlice,
+    startBodySlice + maxSizeBody
+  );
+  const bodyElipsis =
+    (startBodySlice !== 0 ? "..." : "") +
+    bodySlice +
+    (bodySlice.length === maxSizeBody ? "..." : "");
 
-  const { activeNote } = useContext(MainContext);
   return (
     <div
       className={`flex flex-col h-20 px-5 py-2 space-y-1 text-gray-700 ${
@@ -21,12 +42,16 @@ const Note = ({ id, title, body, onClick, onContextMenu }) => {
       onContextMenu={onContextMenu}
     >
       <p className="font-bold">
-        {titleSlice.length === 0
-          ? "No Title"
-          : titleSlice + (titleSlice.length >= maxSizeTitle ? "..." : "")}
+        <Highlighter
+          searchWords={[searchKeyNote]}
+          textToHighlight={titleElipsis}
+        />
       </p>
       <p className="text-xs break-all">
-        {bodySlice + (bodySlice.length >= maxSizeBody ? "..." : "")}
+        <Highlighter
+          searchWords={[searchKeyNote]}
+          textToHighlight={bodyElipsis}
+        />
       </p>
     </div>
   );
